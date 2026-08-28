3ec385fe350e68f13ee3e424f4ce8ebd98682097
jonathan-cruz-dev <136759463+jonathan-cruz-dev@users.noreply.github.com>
2026-08-28 08:48:41 -0300

    Semana 01 - Variables, Constantes y Condicionales

---
+++ semana_01/script.js
@@ -0,0 +1,51 @@
+// alert('"Hola');
+// let nombre = prompt('Ingrese el nombre');
+// let continuar =  confirm('Cerrar sesión');
+const nombre = 'José';  // string cadena
+const dni = 1234; //  Number numero
+let logueado = true;  // Booleano
+
+console.log( typeof(nombre) );
+
+const valor = confirm(' texto ');
+
+const valor = 100;
+const desc = 0.15;
+const res = valor * desc;
+
+
+
+let dato1 = undefined;
+let dato2 = null;
+console.log( typeof(nombre)  );
+console.info();
+console.warn();
+console.error();
+console.table();
+
+console.log(  typeof(nombre), nombre  );
+console.log(  typeof(dni), dni  );
+console.log(  typeof(logueado), logueado  );
+console.log(  typeof(dato1), dato1  );
+console.log(  typeof(dato2), dato2  );
+
+
+
+
+
+
+
+console.log('Hola ' + nombre + ' que tengas un buen día');
+
+// template literal
+console.log(`Hola ${nombre} que tengas un buen día`);
+
+let numero1 = Number( prompt("Ingrese el número") );
+let numero2 = +prompt('Ingrese otro número');
+// let numero2 = parseInt(prompt('Ingrese otro número'));
+
+
+let suma = numero1 + numero2;
+
+alert(`La suma es ${suma}`);
+
